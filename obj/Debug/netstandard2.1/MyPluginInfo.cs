e7d1f97a239f5f69b63eae18ca4a543fb5422753
Andrew N. <bricks041@gmail.com>
2024-06-06 19:31:40 +1000

    remove bin

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace HoloCheck
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "bricks04.holoCheck";
-        public const string PLUGIN_NAME = "HoloCheck";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
